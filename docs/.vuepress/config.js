af15ec1b6a1aa16a26d481a2479a6a8d7ac3a3c1
Natanon <niceday557798@gmail.com>
2021-04-15 02:39:55 +0700

    edit document by natanon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'Co-Vaccine-Moph V.0.1.6 Update 04/04/2021',
+    title: 'Co-Vaccine-Moph V.0.1.7 Update 15/04/2021',
     description: '',
     base: '/co-vaccine-docs/',
     port: '4000',
